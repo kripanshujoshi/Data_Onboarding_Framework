cff4899bbbe927c3e826c1149414f2670a8ce2a2
Kripanshu Joshi <kripanshuj@mastek.com>
2025-05-13 19:09:36 +0100

    Updating config to resolve websocket issue

--- .platform/hooks/postdeploy/01_restart_nginx.sh
+++ .platform/hooks/postdeploy/01_restart_nginx.sh
@@ -3,14 +3,8 @@
 
 echo "Starting postdeploy hook script..."
 
-# Restart Nginx to apply our custom configuration
-if [ -f "/etc/init.d/nginx" ]; then
-  echo "Restarting Nginx service"
-  sudo /etc/init.d/nginx restart || sudo service nginx restart || sudo systemctl restart nginx
-fi
-
-# Create a marker file to indicate our hook ran successfully
-echo "$(date) - Postdeploy hook executed successfully" > /tmp/eb_nginx_postdeploy_success
+# Restart Nginx to apply our WebSocket configuration
+sudo service nginx restart
 
 echo "Postdeploy hook completed"
 exit 0
