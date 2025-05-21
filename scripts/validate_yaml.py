ccaa9399f76ec8356864cb918000cdb57c6f1eb4
Kripanshu Joshi <74981548+kripanshujoshi@users.noreply.github.com>
2025-05-21 22:21:30 +0100

    Merging changes to Dev (#126)
    * fixing cloudformation file path issue in deploy.yml
    
    * fixing cloudformation file path issue in deploy.yml
    
    * fixing cloudformation file path issue in deploy.yml
    
    * fixing using parameter file path issue in deploy.yml
    
    * disabling few code validator workflow by giving arbitrary branch name
    
    * implementing best practises
    
    * implementing best practises
    
    * t rebase --continue
    fixing bugs in workflow
    
    * resolving bugs in pipeline
    
    * Fixed policy validator workflow configuration and added reference policy
    
    ---------
    
    Co-authored-by: Kripanshu Joshi <kripanshuj@mastek.com>

---
+++ scripts/validate_yaml.py
@@ -0,0 +1,11 @@
+import yaml
+import sys
+
+try:
+    with open(sys.argv[1], 'r') as file:
+        yaml.safe_load(file)
+    print("YAML file is valid!")
+except yaml.YAMLError as e:
+    print(f"Error in YAML file: {e}")
+except Exception as e:
+    print(f"Error: {e}")
